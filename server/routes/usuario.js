18fb30f26cbd22ae1fbd2dadc3aeb3019099fa30
joan.montoya <joan15montoya@gmail.com>
2020-12-02 14:22:43 -0600

    Creacion de la carpeta models y primera insercion de datos

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Usuario = require('../models/usuario');
 const app = express();
 
   
@@ -10,21 +11,28 @@ const app = express();
   })
   
   app.post('/usuario', function (req, res) {
-    let nombre=req.body.nombre
     let body = req.body;
-    if(nombre===undefined){
-        res.status(400).json({
-          ok: 400,
-          mensaje: "Favor de enviar el valor del nombre"
-        })
-    }else{
+    let usr = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+    });
+
+    usr.save((err, usrDB) => {
+        if(err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error',
+                err
+            });
+        }
         res.json({
-        ok: "200",
-        mensaje: "usuario insertado con exito",
-        body: body
-      });
-    }  
-  })
+            ok: true,
+            msg: 'Usuario insertado con exito',
+            usrDB
+        });
+    });
+  });
   
   app.put('/usuario/:id/:nombre', function (req, res) {
     let id=req.params.id;
