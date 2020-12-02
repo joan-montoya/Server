f00f88749d155c3dc9ecf76e7deb2df1566c068d
joan.montoya <joan15montoya@gmail.com>
2020-12-02 16:01:53 -0600

    Creamos el DELETE

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -76,12 +76,40 @@ const app = express();
   });
   
   app.delete('/usuario/:id', function (req, res) {
-    let id=req.params.id;
-    res.json({
-      ok: "200",
-      mensaje: "usuario eliminado con exito", 
-      id: id
-    });
-  })
+     /* let id = req.params.id;
+
+      Usuario.deleteOne({ _id: id }, (err, usuarioBorrado) =>{
+        if(err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error al intentar de eliminar el usuario',
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            msg: 'Usuario eliminado con exito',
+            usuarioBorrado
+        });
+      });*/
+      let id =  req.params.id;
+
+      Usuario.findByIdAndUpdate(id, { estado: false }, { new: true, runValidators: true, context: 'query' }, (err, usrDB) => {
+        if(err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error al intentar de eliminar el usuario',
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            msg: 'Usuario eliminado con exito',
+            usrDB
+        });
+      });
+  });
 
 module.exports = app;
